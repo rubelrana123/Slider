0f915b7417214189a77ff22f1d8f0b9e54edafb0
rubelrana123 <96787688+rubelrana123@users.noreply.github.com>
2022-01-21 00:10:53 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+	let wrapper = document.getElementById('wrapper');
+	let topLayer = document.querySelector('.top');
+	let hanlde = document.querySelector('.handle');
+	let skew = 0;
+	let delta = 0;
+
+	if (wrapper.className.indexOf('skewed') != -1) {
+		skew = 990;
+	}
+	wrapper.addEventListener('mousemove', function (e) {
+		delta = (e.clientX - window.innerWidth / 2) * 0.5;
+		hanlde.style.left = e.clientX + delta + 'px';
+		topLayer.style.width = e.clientX + skew + delta + 'px';
+	});
+});
+// <!-----------------------------------------------practice------------------------------------------------------>
+// document.addEventListener('DOMcontentLoaded' , function() {
+//   let wrapper = document.getElementById('wrapper');
+//   let topLayer = document.querySelector('.top');
+//   let handle = document.querySelector('.handle');
+//   let skew = 0;
+//   let delta = 0;
+//   if(wrapper.className.indexOf('skewed')!=-1) {
+//     skew = 980;
+//   }
+//   wrapper.addEventListener('mousemove', function(e) {
+//     delta = (e.clientX - window.innerWidth/2)*0.5;
+//     handle.style.left = e.clientX + delta + 'px';
+//     topLayer.style.width = e.clientX + skew + delta + 'px';
+//   });
+// });
